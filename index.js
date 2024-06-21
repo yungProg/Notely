fcd9beb1c84805908edf6db697a8ee6a36e44f66
yungProg <gboakye007@outlook.com>
2024-06-21 15:49:25 +0000

    partially completed card 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-/* This is an example JavaScript file, feel free to remove/edit it anytime */
-console.log(
-  "%cProject by BigDevSoon",
-  'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
-);
-console.log("Check out more projects at https://bigdevsoon.me");
+const wordsCounted = document.getElementById("word-count-new")
+const description = document.getElementById("form-description-new")
+
+description.addEventListener("input", () => {
+  wordsCounted.textContent = description.value.length
+})
+
